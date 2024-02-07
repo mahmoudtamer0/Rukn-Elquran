b3bf90f68746d59cb955fa4e10eea01f9ce7d981
mahmoud <mamoidtamer300@gmail.com>
2024-02-07 08:58:02 +0200

    .

--- src/components/MainAudio.js
+++ src/components/MainAudio.js
@@ -3,34 +3,33 @@ import "../components/reciters/rec.css"
 import { useData } from '../context/AppContext'
 
 const MainAudio = () => {
-    const { server, soraId, setSoraId } = useData()
+    const { server, setServer, sideBarOpen } = useData()
     const audioRef = useRef()
-    const [audioBool, setAudioBool] = useState(false)
+    const [audioBool, setAudioBool] = useState(true)
 
 
     useEffect(() => {
-        if (soraId == "") {
+        if (server == undefined) {
             setAudioBool(false)
         } else {
             audioRef.current?.play()
             setAudioBool(true)
         }
-    }, [soraId])
+    }, [server])
 
     const handleClick = () => {
-        setSoraId("")
+        setServer()
     }
 
-    // className = { audioBool? "audio": "audioFalse" }
     return (
         < div >
-            <div className={audioBool ? "cancelAudio" : "audioFalse"}><button onClick={() => handleClick()}><i className="fa-solid fa-xmark"></i></button></div>
-            <audio src={`${server}${soraId}.mp3`}
+            <div className={`${audioBool ? "cancelAudio" : "audioFalse"}`}><button onClick={() => handleClick()}><i className="fa-solid fa-xmark"></i></button></div>
+            <audio src={`${server}`}
                 ref={audioRef}
                 type="audio/mpeg"
                 controls className={audioBool ? "audio" : "audioFalse"}>
                 <div>d</div>
-                <source src={`${server}${soraId}.mp3`} type="audio/mpeg" />
+                <source src={`${server}`} type="audio/mpeg" />
                 Your browser does not support the audio tag.
             </audio>
         </div >
