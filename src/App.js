32efc4385f867d48d8a314f5183fc0b078b2fba7
mahmoud <mamoidtamer300@gmail.com>
2024-02-08 08:20:04 +0200

    .

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Sewar from "./components/sewar/Sewar";
 import Sora from "./components/sewar/Sora";
 import MainAudio from "./components/MainAudio";
 import NavBool from "./components/NavBool";
+import Reciters from "./components/reciters/Reciters";
+import Reciter from "./components/reciters/Reciter";
 // import DateToday from "./components/DateToday";
 
 function App() {
@@ -23,6 +25,12 @@ function App() {
               <Sewar />
             </>} />
             <Route path="/Rukn-Elquran/sewar/:soraNum" element={<> <Sora /></>} />
+
+            <Route path="/Rukn-Elquran/reciters" element={<>
+              <Reciters />
+            </>} />
+
+            <Route path="/Rukn-Elquran/reciters/:recId" element={<> <Reciter /></>} />
           </Routes>
           <NavBool />
           <MainAudio />
