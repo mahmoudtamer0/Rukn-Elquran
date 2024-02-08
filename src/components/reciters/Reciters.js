7027ebe76659ea561bb73254b0b0a3677e806725
mahmoud <mamoidtamer300@gmail.com>
2024-02-08 08:21:11 +0200

    .

---
+++ src/components/reciters/Reciters.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { useData } from '../../context/AppContext';
+import "./rec.css"
+import "../sewar/sewar.css"
+import { Link, NavLink } from 'react-router-dom';
+const Reciters = () => {
+
+    const {
+        sewar, getSewar,
+        reciters, getReciters,
+        setServer, setSoraId, colors
+    } = useData()
+    const [sewarList, setSewarList] = useState([])
+    const [newSewar, setNewSewar] = useState([])
+    const [search, setSearch] = useState('')
+
+    const [numberOfRec, setNumberOfRec] = useState(20)
+
+    useEffect(() => {
+        getSewar()
+        getReciters()
+    }, [])
+
+    const handleClick = (rec) => {
+        setServer(rec.moshaf[0].server)
+        setSewarList(rec.moshaf[0].surah_list.split(','))
+    }
+
+
+    useEffect(() => {
+        const newarr = []
+        if (sewarList != '') {
+            sewarList.map(soraList => {
+                sewar?.map(soraName => {
+                    if (soraName.id == soraList) {
+                        newarr.push(soraName)
+                        setNewSewar(newarr)
+                    }
+                })
+            })
+        }
+    }, [sewarList])
+
+
+    const handleSoraClick = (sora) => {
+        const padSora = sora.padStart(3, '0')
+        setSoraId(padSora)
+    }
+
+
+
+
+    return (
+        <div className='mainRec'>
+
+            {/* <div>
+                <input onChange={(e) => setSearch(e.target.value)} type='search' />
+            </div> */}
+            <div className='container'>
+                <div className='sewarBoxes row justify-content-between align-items-center'>
+                    {reciters.filter((item) => {
+                        return search !== "" ? item.name.includes(search) : reciters
+                    }).map((rec, index) => {
+                        return (
+                            <NavLink
+                                to={`/Rukn-Elquran/reciters/${rec.id}`}
+                                className="soraBox d-flex justify-content-between align-items-center"
+                            >
+                                <div className='soraDet'>
+                                    <div style={{ color: colors.searchColor }} className='soraNumberDiv'>
+                                        <span style={{ color: colors.blackColor }} className='soraNumber'>{(index + 1).toLocaleString('ar-EG')}</span>
+                                    </div>
+                                    <span style={{ color: colors.blackColor }} className='soraName'>{rec.name}</span>
+                                </div>
+                                {/* <div className='ayahsCount' style={{ color: colors.greyColor }}>{sora.numberOfAyahs.toLocaleString('ar-EG')}  آيات</div> */}
+                            </NavLink>
+                            // <div style={{ margin: "40px" }} key={rec.id}>
+                            //     <button onClick={() => handleClick(rec)}>{index + 1}-{rec.id} {rec.name}</button>
+                            // </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+            <button onClick={() => setNumberOfRec(225)}>show more</button>
+
+            <hr></hr>
+
+            <div className='recDiv' >
+                {newSewar.map((sora, index) => {
+                    return (
+                        <div style={{ margin: "40px" }} key={sora.id}>
+                            <button onClick={() => handleSoraClick(sora.id.toString())}>{index + 1}-{sora.id} {sora.name}</button>
+                        </div>
+                    )
+                })}
+            </div>
+
+            <hr></hr>
+
+
+        </div >
+
+    )
+}
+export default Reciters;
