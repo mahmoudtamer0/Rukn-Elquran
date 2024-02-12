8d7c5e3b93cceb2848f00a98f4436bc2336be197
mahmoud <mamoidtamer300@gmail.com>
2024-02-12 06:14:18 +0200

    handle customize audio

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import './i18n';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
