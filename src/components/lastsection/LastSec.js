542bb95039d42fddc3a10521216edd9b92cdfc90
mahmoud <mamoidtamer300@gmail.com>
2024-02-16 07:22:53 +0200

    handling text issue

--- src/components/lastsection/LastSec.js
+++ src/components/lastsection/LastSec.js
@@ -13,36 +13,40 @@ export const LastSec = () => {
             <div className='container mainRec' style={{ position: "relative" }}>
                 <h2 style={{ color: colors.blackColor, fontSize: "1.7rem", marginBottom: "40px" }}
                 >{t("home.befor")}</h2>
-                <div className='lastMainBoxes d-flex align-items-center'>
-                    {lastSoras.slice(-10).reverse().map((sora, index) => (
-                        <NavLink
-                            onClick={() => setPageScrollTo(sora.PageNow)}
-                            to={`/Rukn-Elquran/sewar/${sora.soraId}`}
-                            className='mainBox'
-                            style={{ borderColor: colors.borderColor, color: colors.blackColor }}>
-                            <div className='boxHead d-flex justify-content-between align-items-center'>
-                                <span>{sora.soraName}</span>
-                                <span>{sora.soraId}</span>
-                            </div>
-                            <div className='boxBody' style={{ backgroundColor: colors.navColor }}>
-                                <div style={{ fontFamily: font, fontSize: "2rem" }}>{sora.soraName.split("سُورَةُ")}</div>
-                                {sora.PageNow != undefined ?
-                                    <div style={{ fontSize: "17px" }}>
-                                        <span style={{ fontFamily: font }}> {t("home.page")} </span>
-                                        <span> {sora.PageNow}</span>
+                {lastSoras.length > 0 ?
+                    <div className='lastMainBoxes d-flex align-items-center'>
+                        {lastSoras.slice(-10).reverse().map((sora, index) => (
+                            <NavLink
+                                onClick={() => setPageScrollTo(sora.PageNow)}
+                                to={`/Rukn-Elquran/sewar/${sora.soraId}`}
+                                className='mainBox'
+                                style={{ borderColor: colors.borderColor, color: colors.blackColor }}>
+                                <div className='boxHead d-flex justify-content-between align-items-center'>
+                                    <span>{sora.soraName}</span>
+                                    <span>{sora.soraId}</span>
+                                </div>
+                                <div className='boxBody' style={{ backgroundColor: colors.navColor }}>
+                                    <div style={{ fontFamily: font, fontSize: "2rem" }}>{sora.soraName.split("سُورَةُ")}</div>
+                                    {sora.PageNow != undefined ?
+                                        <div style={{ fontSize: "17px" }}>
+                                            <span style={{ fontFamily: font }}> {t("home.page")} </span>
+                                            <span> {sora.PageNow}</span>
 
-                                    </div>
-                                    :
-                                    <div style={{ fontSize: "17px" }}>
-                                        <span style={{ fontFamily: font }}> {t("home.page")} </span>
-                                        <span> {sora.firstPageNumber}</span>
-                                    </div>
-                                }
+                                        </div>
+                                        :
+                                        <div style={{ fontSize: "17px" }}>
+                                            <span style={{ fontFamily: font }}> {t("home.page")} </span>
+                                            <span> {sora.firstPageNumber}</span>
+                                        </div>
+                                    }
 
-                            </div>
-                        </NavLink>
-                    ))}
-                </div>
+                                </div>
+                            </NavLink>
+                        ))}
+                    </div>
+                    :
+                    <div style={{ color: colors.greyColor, fontSize: "20px" }}>{t("home.no_surahs")}</div>
+                }
             </div>
         </div>
     )
