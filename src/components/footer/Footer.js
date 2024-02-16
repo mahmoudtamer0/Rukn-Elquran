afbd43826f4856e04df9d170830f5e10ada55a18
mahmoud <mamoidtamer300@gmail.com>
2024-02-16 06:15:54 +0200

    handling multi languages

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import android from '../../images/android.svg'
 import apple from '../../images/apple.svg'
 import { useData } from '../../context/AppContext';
+import { NavLink } from 'react-router-dom';
 function Footer() {
     const { t, i18n } = useTranslation();
     const { colors } = useData()
@@ -12,25 +13,25 @@ function Footer() {
                 <div className='container'>
                     <div className='row foot-sec-main justify-content-between align-items-center'>
                         <div className='secondlayer-firstdiv col-lg-2 col-md-4 col-sm-12 col-12'>
-                            <h4>{t("footer.about_store")}</h4>
+                            <h4 style={{ color: colors.blackColor }}>{t("footer.about_store")}</h4>
                             <p>{t("footer.about_desc")}</p>
                         </div>
                         <div className='secondlayer-secdiv col-lg-2 col-md-4 col-sm-12 col-12'>
-                            <h4>{t("footer.col2_title")}</h4>
+                            <h4 style={{ color: colors.blackColor }}>{t("footer.col2_title")}</h4>
                             <ul>
-                                <li><i className="fa-solid fa-angles-left"></i> <a>{t("footer.col2_link1")}</a> </li>
-                                <li><i className="fa-solid fa-angles-left"></i> <a>{t("footer.col2_link2")}</a> </li>
-                                <li><i className="fa-solid fa-angles-left"></i> <a>{t("footer.col2_link3")}</a> </li>
-                                <li><i className="fa-solid fa-angles-left"></i> <a>{t("footer.col2_link4")}</a> </li>
+                                <li><i className="fa-solid fa-angles-left"></i> <NavLink to={`/Rukn-Elquran`}>{t("footer.col2_link1")}</NavLink> </li>
+                                <li><i className="fa-solid fa-angles-left"></i> <NavLink to={`/Rukn-Elquran/radio`}>{t("footer.col2_link2")}</NavLink> </li>
+                                <li><i className="fa-solid fa-angles-left"></i> <NavLink to={`/Rukn-Elquran/reciters`}>{t("footer.col2_link3")}</NavLink> </li>
+                                <li><i className="fa-solid fa-angles-left"></i> <NavLink to={`/Rukn-Elquran/sewar`}>{t("footer.col2_link4")}</NavLink> </li>
                             </ul>
                         </div>
                         <div className='secondlayer-thrdiv col-lg-2 col-md-4 col-sm-12 col-12'>
-                            <h4>{t("footer.col3_title")}</h4>
+                            <h4 style={{ color: colors.blackColor }}>{t("footer.col3_title")}</h4>
                             <ul>
-                                <li><i className="fa-solid fa-angles-left"></i> <a>{t("footer.col3_link1")}</a> </li>
-                                <li><i className="fa-solid fa-angles-left"></i> <a>{t("footer.col3_link2")}</a> </li>
-                                <li><i className="fa-solid fa-angles-left"></i> <a>{t("footer.col3_link3")}</a> </li>
-                                <li><i className="fa-solid fa-angles-left"></i> <a>{t("footer.col3_link4")}</a> </li>
+                                <li><i className="fa-solid fa-angles-left"></i> <NavLink to={`/Rukn-Elquran/sewar/36`}>{t("footer.col3_link1")}</NavLink> </li>
+                                <li><i className="fa-solid fa-angles-left"></i> <NavLink to={`/Rukn-Elquran/sewar/67`}>{t("footer.col3_link2")}</NavLink> </li>
+                                <li><i className="fa-solid fa-angles-left"></i> <NavLink to={`/Rukn-Elquran/reciters/107`}>{t("footer.col3_link3")}</NavLink> </li>
+                                <li><i className="fa-solid fa-angles-left"></i> <NavLink to={`/Rukn-Elquran/sewar/2`}>{t("footer.col3_link4")}</NavLink> </li>
                             </ul>
                         </div>
                     </div>
@@ -47,7 +48,7 @@ function Footer() {
                         </div>
                         <div className='foot-madby col-lg-3'>
                             <h4 style={{ color: colors.blackColor, fontSize: "14px", margin: "0" }}>Made By:
-                                <a style={{ color: colors.mainColor }}
+                                <a className='mahmoud' style={{ color: colors.mainColor }}
                                     href='https://www.linkedin.com/in/mahmoudtamer0' target='_blank'
                                 > Mahmoud Tamer </a></h4>
                         </div>
