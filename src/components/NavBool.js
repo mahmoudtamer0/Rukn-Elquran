f7e19d8dc00ff7355e12657397aa48242f24ab71
mahmoud <mamoidtamer300@gmail.com>
2024-02-18 06:20:39 +0200

    Adding azkar page

--- src/components/NavBool.js
+++ src/components/NavBool.js
@@ -4,12 +4,12 @@ import { useLocation } from "react-router-dom"
 import { useData } from "../context/AppContext"
 
 const NavBool = () => {
-    const { setScrollBool } = useData()
+    const { setScrollBool, setAzkarBool } = useData()
     const { pathname } = useLocation()
 
     useEffect(() => {
-        if (pathname.includes("/Rukn-Elquran/sewar/")) {
-            return
+        if (pathname.includes("/Rukn-Elquran/sewar/") || pathname.includes("/Rukn-Elquran/azkar")) {
+            setScrollBool(true)
         } else {
             setScrollBool(false)
         }
@@ -17,6 +17,7 @@ const NavBool = () => {
         if (pathname.includes("/Rukn-Elquran/sewar/")) {
             return
         } else window.scrollTo(0, 0);
+
     }, [pathname])
 }
 
