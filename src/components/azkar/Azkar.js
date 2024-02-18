8e5adcc395deb2bda11d9652530f5c5ac41383a4
mahmoud <mamoidtamer300@gmail.com>
2024-02-18 06:20:59 +0200

    Adding azkar page

---
+++ src/components/azkar/Azkar.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useRef, useState, useTransition } from 'react'
+import { Link, useParams, NavLink } from 'react-router-dom'
+import { useData } from '../../context/AppContext'
+import '../sewar/sewar.css'
+import { useLocation } from 'react-router-dom'
+import { useClickAway } from "@uidotdev/usehooks";
+import MoonLoader from "react-spinners/MoonLoader";
+import PulseLoader from "react-spinners/PulseLoader";
+import { useTranslation } from 'react-i18next'
+
+export const Azkar = () => {
+    const {
+        ayahs,
+        colors,
+        lang,
+        reciters,
+        font,
+        setServer,
+        server,
+        lastSoras,
+        setLastSoras,
+        pageScrollTo
+    } = useData()
+    const { t, i18n } = useTranslation()
+    const [azkar, setAzkar] = useState([])
+    const [scrollY, setScrollY] = useState(0)
+    const zekrs = [...document.querySelectorAll('.soraPage')];
+    const [pageNow, setPageNow] = useState()
+    const [soraName, setSoraName] = useState("اذكار")
+
+    useEffect(() => {
+
+        fetch(`https://www.hisnmuslim.com/api/en/27.json`)
+            .then(res => res.json())
+            .then(data => setAzkar(data[Object.keys(data)[0]]))
+
+    }, [])
+
+    const handlePlayClick = (audio) => {
+        setServer(audio)
+    }
+
+
+    useEffect(() => {
+        const index = lastSoras.findIndex((sora) => sora.soraName === soraName)
+        if (index !== -1) {
+            const updatedProducts = [...lastSoras.slice(0, index), ...lastSoras.slice(index + 1), {
+                soraName: soraName,
+                soraId: pageNow,
+                firstPageNumber: 1,
+                PageNow: pageNow
+            }];
+            setLastSoras(updatedProducts)
+        } else {
+            setLastSoras([...lastSoras, {
+                firstPageNumber: 0,
+                soraName: soraName,
+                soraId: pageNow,
+                PageNow: pageNow
+            }])
+        }
+    }, [scrollY])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY)
+            if (zekrs.length == 0) {
+                return
+            } else {
+                zekrs.map(page =>
+                (
+                    page.offsetTop - 50 <= scrollY &&
+                    setPageNow(page?.className.split("soraPage zekr")[1])
+                )
+                )
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [scrollY])
+
+
+    useEffect(() => {
+        window.scrollTo(0, document.querySelector(`.zekr${pageScrollTo}`)?.offsetTop - 50)
+    }, [azkar])
+    return (
+        <div
+            className={`Sora`} style={{ marginRight: "0" }}>
+            <div className='soraContent'>
+                <div className='besmAllah'>
+                    <h2
+                        style={{ color: colors.blackColor, fontFamily: font }}
+                        className={`text-center ${lang == "eng" && "h2Eng"}`}>{t("home.azkar")}</h2>
+                </div>
+
+                <>
+                    <div>
+                        {azkar?.map((zekr, index) => (
+                            < div
+                                style={{ borderBottom: `1px solid ${colors.borderColor}` }}
+                                className={`soraPage zekr${index + 1}`}
+                                key={zekr.ID}>
+                                <div className='audioButton'>
+                                    {server !== zekr.AUDIO ?
+                                        <button onClick={() => handlePlayClick(zekr.AUDIO)}>
+                                            <i className="fa-solid fa-play"></i>  {t("home.play_azkar_btn")}
+                                        </button>
+                                        :
+                                        <button className='audioStop' onClick={() => setServer("")}>
+                                            <i className="fa-solid fa-stop"></i>   {t("sora.pause_btn")}
+                                        </button>
+                                    }
+
+                                </div>
+                                <p style={{ color: colors.blackColor }} className={`pAyah ${lang == "eng" && "ayaheng"}`}>
+                                    <span>
+                                        <span
+
+                                            className={`ayahSpan `}>
+                                            {lang == "ar" ? zekr.ARABIC_TEXT : zekr.TRANSLATED_TEXT}
+                                        </span>
+                                    </span>
+                                </p>
+                                <div style={{ color: colors.blackColor }} className='text-center mt-3'> <span>{(index + 1).toLocaleString("ar-EG")}</span> </div>
+                            </div>
+                        ))}
+                        {/* {Object.keys(pages).map(pageNumber => (
+                            
+                        ))} */}
+                    </div>
+                </>
+            </div>
+        </div >
+    )
+}
