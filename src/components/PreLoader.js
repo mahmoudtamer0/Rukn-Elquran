4fd6d926ff8c7d2c9188592e51db59f4456b67d6
mahmoud <mamoidtamer300@gmail.com>
2024-02-19 20:49:49 +0200

    adding preloader

---
+++ src/components/PreLoader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PreLoader = () => {
+    return (
+        <div className="preloader">
+            <div className="preloaderText">
+                <span className="ruknSpan"> رُكْنُ </span>
+                <span className="quranSpan"> اَلْقُرْآنِ</span>
+            </div>
+            <div className="bySpan">by Mahmoud Tamer</div>
+        </div>
+    )
+}
+
+export default PreLoader
