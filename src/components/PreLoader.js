1c8bc2ee053840a7aa941f3e99be1e789131e0b7
mahmoud <mamoidtamer300@gmail.com>
2024-02-20 10:11:32 +0200

    maskable icon

--- src/components/PreLoader.js
+++ src/components/PreLoader.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const PreLoader = () => {
+    const [loading, setLoading] = useState(false)
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        if (pathname.includes("/Rukn-Elquran/sewar/")) {
+            setLoading(false)
+        } else {
+            setLoading(true)
+            setTimeout(() => {
+                setLoading(false)
+            }, 1800)
+        }
+    }, [])
     return (
-        <div className="preloader">
+        <div className={`preloader ${!loading && "d-none"}`}>
             <div className="preloaderText">
                 <span className="ruknSpan"> رُكْنُ </span>
                 <span className="quranSpan"> اَلْقُرْآنِ</span>
