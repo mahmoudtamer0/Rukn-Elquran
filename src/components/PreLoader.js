6171bec1bf0971a5ef80b7e4f985049a6bcfc441
mahmoud <mamoidtamer300@gmail.com>
2024-02-20 11:43:39 +0200

    preloader background

--- src/components/PreLoader.js
+++ src/components/PreLoader.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useLocation } from 'react-router-dom'
-
+import { useData } from '../context/AppContext'
 const PreLoader = () => {
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
     const { pathname } = useLocation()
+    const { t, i18n } = useTranslation()
+    const { font, colors } = useData()
 
     useEffect(() => {
         if (pathname.includes("/Rukn-Elquran/sewar/")) {
@@ -16,10 +19,10 @@ const PreLoader = () => {
         }
     }, [])
     return (
-        <div className={`preloader ${!loading && "d-none"}`}>
+        <div className={`preloader ${!loading && "d-none"}`} style={{ backgroundColor: colors.preLoader }}>
             <div className="preloaderText">
-                <span className="ruknSpan"> رُكْنُ </span>
-                <span className="quranSpan"> اَلْقُرْآنِ</span>
+                <span className="ruknSpan" style={{ fontFamily: font }}> {t("navBar.logo1")} </span>
+                <span className="quranSpan" style={{ fontFamily: font }}> {t("navBar.logo2")} </span>
             </div>
             <div className="bySpan">by Mahmoud Tamer</div>
         </div>
