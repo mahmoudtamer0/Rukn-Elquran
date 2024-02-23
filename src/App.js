dd07146ce4722375241f15740118de164519abfb
mahmoud <mamoidtamer300@gmail.com>
2024-02-23 03:39:17 +0200

    .

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     <div className="App" >
       <Analytics />
       <SpeedInsights />
-      <Router basename="/Rukn-Elquran">
+      <Router basename="/">
         <Analytics />
         <SpeedInsights />
         <AppContext>
