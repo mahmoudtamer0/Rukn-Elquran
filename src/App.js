abdbc00a9d7bad8953b1b849e5d9f5bc9c2603aa
mahmoud <mamoidtamer300@gmail.com>
2024-03-02 05:00:08 +0200

    handle font error

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="App" >
-      <Router basename="/Rukn-Elquran">
+      <Router>
         <Analytics />
         <SpeedInsights />
         <AppContext>
